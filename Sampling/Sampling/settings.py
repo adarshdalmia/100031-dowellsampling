b92358dcc32ba03f6feac40f476ec3982cc0f405
Akash Verma <96557682+gitone912@users.noreply.github.com>
2023-05-18 18:46:07 +0530

    Update settings.py corsheaders

--- Sampling/Sampling/settings.py
+++ Sampling/Sampling/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'API',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'Sampling.urls'
