cb39def4abdc3b832d1b63508d805422a3df769a
gitone912 <akash.101@kvsroranchi.in>
2023-06-12 19:07:29 +0530

    updates

--- Sampling/API/views.py
+++ Sampling/API/views.py
@@ -12,6 +12,45 @@ from API.functions.clusterSampling import dowellClusterSampling
 from API.functions.purposiveSampling import dowellPurposiveSampling
 
 
+import json
+import requests
+import pprint
+
+def get_event_id():
+
+    url="https://uxlivinglab.pythonanywhere.com/create_event"
+
+    data={
+        "platformcode":"FB" ,
+        "citycode":"101",
+        "daycode":"0",
+        "dbcode":"pfm" ,
+        "ip_address":"192.168.0.41", # get from dowell track my ip function 
+        "login_id":"lav", #get from login function
+        "session_id":"new", #get from login function
+        "processcode":"1",
+        "location":"22446576", # get from dowell track my ip function 
+        "objectcode":"1",
+        "instancecode":"100051",
+        "context":"afdafa ",
+        "document_id":"3004",
+        "rules":"some rules",
+        "status":"work",
+        "data_type": "learn",
+        "purpose_of_usage": "add",
+        "colour":"color value",
+        "hashtags":"hash tag alue",
+        "mentions":"mentions value",
+        "emojis":"emojis",
+        "bookmarks": "a book marks"
+    }
+
+    r=requests.post(url,json=data)
+    if r.status_code == 201:
+        return json.loads(r.text)
+    else: 
+        return json.loads(r.text)['error']
+
 @csrf_exempt
 def get_data(request):
     header = { 'content-type': 'application/json'}
@@ -204,10 +243,12 @@ def stratified_sampling(request):
         }
 
         samples = dowellStratifiedSampling(stratifiedSamplingInput)
+        id = get_event_id()
         response = {
             'samples': samples
         }
-        response = {"samples": samples['sampleUnits']}
+        response = {"event_id":id['event_id'],"samples": samples['sampleUnits']}
+        
         if result == 'Table':
             return render(request, 'result.html', {'response': response})
         return JsonResponse(response, safe=False)
