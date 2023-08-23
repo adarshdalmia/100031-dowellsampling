38c21b6129023dea05865c0ac78df4629ef037d6
Hammad Rao <106843117+hammadrao891@users.noreply.github.com>
2023-08-23 18:06:51 +0500

    Delete Sampling directory

--- Sampling/API/functions/randomGeneration.py
+++
@@ -1,6 +0,0 @@
-from random import randrange
-
-def dowellRandomGeneration(N, n, Yi):
-    rand_nos = [randrange(0, N) for _ in range(n)]
-    sampleUnits = [Yi[i] for i in rand_nos] 
-    return sampleUnits
--- Sampling/API/functions/sampleSize.py
+++
@@ -1,6 +0,0 @@
-def dowellSampleSize(N, e):
-    n = int(N / (1 + N * e * e))
-    if n > 30 and n < 500:
-        return n
-    else:
-        return(f"Sample size is not adequate")
