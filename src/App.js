adf34fe2ff0e4ee733a4f54e1beac8c86900b930
hammadrao891 <hammadrao891@gmail..com>
2023-08-24 17:45:33 +0500

    nip

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Search from './components/Search';
-import Main from './pages/Main';
-import Rough from './pages/Rough';
 import SampleSize from './components/SampleSize';
 
 function App() {
   return (
     <div>
       <SampleSize/>
-      {/* <Search/> */}
-      {/* <Main/> */}
-    {/* <Rough/> */}
     </div>
   );
 }
