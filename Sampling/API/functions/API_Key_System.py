50882110a76d7339f7f1f1a530eb9d5bf4e9b975
Ritik Chahar <102214612+RitikChahar@users.noreply.github.com>
2023-08-29 16:50:02 +0530

    Updated the API Key system

--- Sampling/API/functions/API_Key_System.py
+++ Sampling/API/functions/API_Key_System.py
@@ -1,10 +1,9 @@
 import requests
 
 def processApikey(api_key, api_service):
-    url = 'https://100105.pythonanywhere.com/api/v1/process-api-key/'
+    url = f'https://100105.pythonanywhere.com/api/v3/process-services/?type=api_service&api_key={api_key}'
     payload = {
-        "api_key" : api_key,
-        "api_service_id" : api_service
+        "service_id" : api_service
     }
     response = requests.post(url, json=payload)
     return response.text
